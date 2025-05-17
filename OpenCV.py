103e8d0a1e38e8e1fd68ac1382d558dfc1c1abf2
Jayden <153332084+zFa3@users.noreply.github.com>
2025-05-16 23:10:30 -0400

    hopefully my api key is not here

--- OpenCV.py
+++ OpenCV.py
@@ -7,4 +7,6 @@ while capture.isOpened():
     successful, frame = capture.read()
     frame = cv2.flip(frame, 1)
 
-    
+    if not successful: continue # frame not recieved
+
+    rgb_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
